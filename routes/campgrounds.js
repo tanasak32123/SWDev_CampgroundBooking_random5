1c973901f4e4b01e3a4553190aaea81d18784c21
Alongkot <alongkot317@gmail.com>
2023-04-11 15:01:21 +0700

    implement CRUD for Campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const {getCampgrounds,getCampground,createCampground,updateCampground,deleteCampground} = require('../controllers/campgrounds');
 
-
+router.route('/').get(getCampgrounds).post(createCampground);
+router.route('/:id').get(getCampground).put(updateCampground).delete(deleteCampground);
 
 module.exports=router;

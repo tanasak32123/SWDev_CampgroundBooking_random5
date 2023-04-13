556d2f1d21b3d00a8c10b5dda1f10b279b302dcd
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-04-13 14:50:54 +0700

    latest auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,27 @@
 const User = require("../models/User");
 
+const sendTokenResponse = (user, statusCode, res) => {
+  //Create token
+  const token = user.getSignedJwtToken();
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: true, token });
+};
+
 //@desc     Register user
-//@route    POST /api/v1/auth/register
+//@route    POST /api/v5/auth/register
 //@access   Public
 exports.register = async (req, res, next) => {
   try {
@@ -15,9 +35,68 @@ exports.register = async (req, res, next) => {
       password,
       role,
     });
-    res.status(200).json({ success: true });
+
+    sendTokenResponse(user, 200, res);
   } catch (err) {
     res.status(400).json({ success: false });
     console.log(err.stack);
   }
+};
+
+//@desc     Login user
+//@route    POST /api/v5/auth/login
+//@access   Public
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    //Validate email & password
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, msg: `Please provide an email and password` });
+    }
+
+    //Check for user
+    const user = await User.findOne({ email }).select(`+password`);
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, msg: `Invalid credentials` });
+    }
+
+    //Check if password is matched
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ success: false, msg: "Invalid credentials" });
+    }
+
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      msg: "Cannot convert email or password to string",
+    });
+  }
+};
+
+exports.getMe = async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  return res.status(200).json({ success: true, data: user });
+};
+
+exports.logout = async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
 };

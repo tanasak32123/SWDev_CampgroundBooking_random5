6ee4c187ab48db2b467fa39d05041bd36ec0b05e
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-04-22 19:47:09 +0700

    change word && set default show average

--- backend/routes/status.js
+++ backend/routes/status.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const {getStatus,getAverage, getRecommendation} = require('../controllers/status')
-const router = express.Router({mergeParams:true});
+const express = require("express");
+const {
+  getStatus,
+  getAverage,
+  getRecommendation,
+} = require("../controllers/status");
+const router = express.Router({ mergeParams: true });
 
-router.route('/:date').get(getStatus);
-router.route('/average/:dayNumber').get(getAverage);
-router.route('/').get(getRecommendation);
+router.route("/:date").get(getStatus);
+router.route("/average/:dayNumber").get(getAverage);
+router.route("/").get(getRecommendation);
 
-module.exports=router;
+module.exports = router;
